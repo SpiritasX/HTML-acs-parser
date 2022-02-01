69abe5016614528cbe3648337cac32c2d4eb37d7
SpiritasX <32173996+SpiritasX@users.noreply.github.com>
2022-02-01 17:11:55 +0100

    Update autorun.bat

--- HTML__acs__parser/autorun.bat
+++ HTML__acs__parser/autorun.bat
@@ -1,6 +1,8 @@
 @echo off
 
 Rem https://stackoverflow.com/a/58530843
+Rem https://stackoverflow.com/a/6362922
+Rem https://stackoverflow.com/a/58882066
 
 SET command=python C:\Users\pompeii\Source\Repos\SpiritasX\HTML__acs__parser\HTML__acs__parser\HTML__acs__parser.py %1
 
